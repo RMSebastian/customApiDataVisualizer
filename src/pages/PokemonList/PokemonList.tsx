7cf557e14e0decbcb74d4e1abd60e34d3c3bd5ae
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2024-07-15 10:16:27 -0300

    infiniteScroll

--- src/pages/PokemonList/PokemonList.tsx
+++ src/pages/PokemonList/PokemonList.tsx
@@ -1,15 +1,15 @@
 import { Pokemon, PokemonListApiResponse } from "../../interfaces/Pokemon";
 import ListGroup from "../../components/ListGroup/ListGroup";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./PokemonList.css";
 
 const PokemonList = () => {
   const [pages, setPages] = useState<number>(0);
   const [paginatePokemon, SetPaginatePokemon] =
     useState<PokemonListApiResponse | null>(null);
-  const [detailPokemon, SetDetailPokemon] = useState<Pokemon[] | null>(null);
+  const [detailPokemon, SetDetailPokemon] = useState<Pokemon[]>([]);
   const itemsPerPage: number = 20;
-  useEffect(() => {
+  const fetchPokemon = useCallback(()=>{
     fetch(
       `https://pokeapi.co/api/v2/pokemon/?limit=${itemsPerPage}&offset=${
         pages * itemsPerPage
@@ -21,32 +21,45 @@ const PokemonList = () => {
       })
       .catch((err) => console.log(err));
   }, [pages]);
-  useEffect(() => {
+  const fetchDetailPokemon = useCallback(() =>{
     if (paginatePokemon != null) {
       const fetchingDetails = async () => {
         const newPromise = paginatePokemon.results.map((resource) =>
           fetch(resource.url).then((response) => response.json())
         );
         const pokemons: Pokemon[] = await Promise.all(newPromise);
-        SetDetailPokemon(pokemons);
+        SetDetailPokemon((prev) => [...prev, ...pokemons]);
       };
       fetchingDetails();
     }
-  }, [paginatePokemon]);
+  }, [paginatePokemon])
+  const handleScroll = useCallback(() => {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+    setPages((prevPages) => prevPages + 1);
+  }, []);
+
+  useEffect(() => {
+    fetchPokemon();
+  }, [fetchPokemon]);
+  useEffect(() => {
+    fetchDetailPokemon();
+  }, [fetchDetailPokemon]);
+  useEffect(() => {
+    
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+}, [handleScroll]);
 
   if (!detailPokemon) return <div>Loading...</div>;
   return (
     <div className="app">
-      <div>
-        <ListGroup key={1} items={detailPokemon} heading="Pokemon"></ListGroup>
+      <div className="header">
+        <h1>Header</h1>
       </div>
       <div>
-        {paginatePokemon?.previous != null && (
-          <button onClick={() => setPages(pages - 1)}>{"previous"}</button>
-        )}
-        {paginatePokemon?.next != null && (
-          <button onClick={() => setPages(pages + 1)}>{"next"}</button>
-        )}
+        <ListGroup key={1} items={detailPokemon} heading="Pokemon"></ListGroup>
       </div>
     </div>
   );
